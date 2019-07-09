7605ae819ca84cd6167bb7a20552be4b2be59289
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-07-09 14:48:58 +0100

    feat: implement delete post

--- routes/index.js
+++ routes/index.js
@@ -43,4 +43,29 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+router.delete('/:id', async (req, res) => {
+  const id = Number(req.params.id);
+  if (Number.isNaN(id) || id % 1 !== 0 || id < 0) {
+    return res.status(400).send({
+      message: 'The post ID provided is not valid',
+    });
+  }
+  try {
+    const deleteResponse = await db.remove(id);
+    if (deleteResponse === 1) {
+      return res.status(200).json({
+        message: 'Post deleted successfully',
+      });
+    }
+    return res.status(404).json({
+      message: 'The post with the specified ID does not exist.',
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      error: 'The post could not be removed',
+    });
+  }
+});
+
 module.exports = router;
